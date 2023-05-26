93278d23974ef08a9ee4a9c40acffcea30f6603c
syg <sygint@users.noreply.github.com>
2023-05-26 02:53:14 -0700

    add init and remote origin change to install.sh

--- install.sh
+++ install.sh
@@ -5,3 +5,8 @@ set -e # -e: exit on error
 if [ ! "$(command -v chezmoi)" ]; then
   sudo pacman -S chezmoi
 fi
+
+chezmoi init --apply https://github.com/sygint/dotfiles.git
+chezmoi cd
+git remote remove origin
+git remote add origin git@github.com:SYGINT/dotfiles.git
