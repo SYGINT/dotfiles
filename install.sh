f84a87dc29aa45f07fc2b6539b1fd4fbf9d8b16f
Syg <872633+SYGINT@users.noreply.github.com>
2023-06-03 19:06:23 -0700

    update install.sh

--- install.sh
+++ install.sh
@@ -13,6 +13,7 @@ sudo pacman -S artix-archlinux-support
 
 # Edit pacman.d mirror list to include arch repos
 sudo sed -i '$!N;s/\[galaxy\]\nInclude = \/etc\/pacman\.d\/mirrorlist/&\n\n\[extra\]\nInclude = \/etc\/pacman\.d\/mirrorlist-arch\n\n\[community\]\nInclude = \/etc\/pacman\.d\/mirrorlist-arch/;P;D' /etc/pacman.conf
+sudo pacman -Sy
 
 if [ ! "$(command -v chezmoi)" ]; then
   sudo pacman -S chezmoi
