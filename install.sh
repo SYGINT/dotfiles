833de0085162e898d23aadffd5781dec63dc17f8
Syg <872633+SYGINT@users.noreply.github.com>
2023-06-03 22:22:27 -0700

    update install.sh

--- install.sh
+++ install.sh
@@ -2,11 +2,32 @@
 
 set -e # -e: exit on error
 
+universe="
+[universe]
+Server = https://universe.artixlinux.org/\$arch
+Server = https://mirror1.artixlinux.org/universe/\$arch
+Server = https://mirror.pascalpuffke.de/artix-universe/\$arch
+Server = https://mirrors.qontinuum.space/artixlinux-universe/\$arch
+Server = https://mirror1.cl.netactuate.com/artix/universe/\$arch
+Server = https://ftp.crifo.org/artix-universe/\$arch
+Server = https://artix.sakamoto.pl/universe/\$arch"
+
+arch_repos="
+[extra]
+Include = /etc/pacman.d/mirrorlist-arch
+
+[community]
+Include = /etc/pacman.d/mirrorlist-arch"
+
 # Update Repositories and Packages
 sudo pacman -Syu
 
 # Add needed packages
 sudo pacman -S base-devel
+
+# Add universe repos
+echo "${universe}" | sudo tee -a /etc/pacman.conf
+sudo pacman -Sy
 
 # Add Arch support
 sudo pacman -S artix-archlinux-support
